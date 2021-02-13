a89415b5fcad5ef4d9c70d496b34461980d50504
Christoffel Angga <chris.ang.dev@gmail.com>
2021-02-13 09:55:22 -0800

    MERN boilerplate and dev environment setup.  Please enter the commit message for your changes. Lines starting

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client', 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.(jsx|js)$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+};
